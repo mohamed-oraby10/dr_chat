cfbaae117daa13ad3b0a6e68c8c56d3b7992826e
hp <mohamedoraby0500@gmail.com>
2025-10-29 15:09:04 +0300

    build structure of app

---
+++ lib/core/utils/routes.dart
@@ -0,0 +1,8 @@
+import 'package:dr_chat/Features/Splash/presentation/views/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static final router = GoRouter(
+    routes: [GoRoute(path: '/', builder: (context, state) => SplashView())],
+  );
+}
