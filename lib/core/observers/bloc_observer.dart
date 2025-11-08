e348e1c7736bc9615f86a41e9f4ae7d1eed89df9
hp <mohamedoraby0500@gmail.com>
2025-11-08 12:47:02 +0200

    create simple bloc observer

---
+++ lib/core/observers/bloc_observer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    debugPrint('change = $bloc');
+  }
+
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    debugPrint('create = $bloc');
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    debugPrint('close = $bloc');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:new_dr_chat_application/core/observers/bloc_observer.dart';
 import 'package:new_dr_chat_application/core/utils/routes.dart';
 import 'package:new_dr_chat_application/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // setupServiceLocator();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  Bloc.observer = SimpleBlocObserver();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const DrChat());
 }
 
